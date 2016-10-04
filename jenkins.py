8565c03fa21a5edaced084933ce2230fe670e688
Ruslan Baratov <ruslan_baratov@yahoo.com>
2016-10-04 12:17:38 +0300

    verbose=False

--- jenkins.py
+++ jenkins.py
@@ -132,6 +132,12 @@ def run():
   ):
     verbose = False
 
+  if (
+      os.getenv('TRAVIS') and
+      (project_dir == 'examples/hdf5')
+  ):
+    verbose = False
+
   project_dir = os.path.join(cdir, project_dir)
   project_dir = os.path.normpath(project_dir)
 
