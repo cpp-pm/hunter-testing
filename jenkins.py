81401228e09ebce37a9635b7469596a7aa6f79be
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-03-28 13:45:53 +0800

    Add 'verbose=False'

--- jenkins.py
+++ jenkins.py
@@ -132,6 +132,12 @@ def run():
   ):
     verbose = False
 
+  if (
+      os.getenv('TRAVIS') and
+      (project_dir == 'examples/imshow')
+  ):
+    verbose = False
+
   project_dir = os.path.join(cdir, project_dir)
   project_dir = os.path.normpath(project_dir)
 
