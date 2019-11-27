5f3fcb3cfb73add55b6d9a371ffcf195713ebbc6
Kai Howelmeyer <kai@hoewelmeyer.eu>
2019-11-27 20:46:55 +0100

    Update sqlite3 to 3.30.1 (#78)

--- cmake/configs/default.cmake
+++ cmake/configs/default.cmake
@@ -470,7 +470,7 @@ else()
 endif()
 
 hunter_default_version(spirv-cross VERSION 20190906)
-hunter_default_version(sqlite3 VERSION 3.27.2-p0)
+hunter_default_version(sqlite3 VERSION 3.30.1-p0)
 hunter_default_version(sse2neon VERSION 1.0.0-p0)
 hunter_default_version(stanhull VERSION 0.0.1)
 hunter_default_version(state_machine VERSION 1.1)
