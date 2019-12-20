08b21341fb84199da619e7f5cfcf863554cb7e5c
Joerg-Christian Boehme <joerg@chaosdorf.de>
2019-12-20 17:20:43 +0100

    Add 'Zug' package (#107)
    * Initial commit
    
    * First working version
    
    * Cleanup
    
    * Reorder build.
    
    * Add pull request number

--- cmake/configs/default.cmake
+++ cmake/configs/default.cmake
@@ -160,6 +160,7 @@ hunter_default_version(YAJL VERSION 2.1.0-p0)
 hunter_default_version(ZLIB VERSION 1.2.11-p1)
 hunter_default_version(ZMQPP VERSION 4.2.0-p0)
 hunter_default_version(ZeroMQ VERSION 4.2.3-p1)
+hunter_default_version(Zug VERSION 0.0.1-be20cae)
 
 hunter_default_version(acf VERSION 0.1.14)
 hunter_default_version(actionlib VERSION 1.11.13-p0)
