45fef6e38a2aeb3fe2c62b4cfff17518feb65fc5
Rahul Sheth <rahul@snap.com>
2020-08-13 18:20:14 -0400

    Update 're2' to v2020.08.01 (#240)
    * Update 're2' to v2020.08.01
    
    * Try changing target in example
    
    * Disable testing

--- cmake/configs/default.cmake
+++ cmake/configs/default.cmake
@@ -441,7 +441,7 @@ else()
   hunter_default_version(range-v3 VERSION 0.5.0)
 endif()
 
-hunter_default_version(re2 VERSION 2017.11.01-p0)
+hunter_default_version(re2 VERSION 2020.08.01)
 hunter_default_version(readline VERSION 6.3)
 hunter_default_version(recastnavigation VERSION 1.4-p0)
 hunter_default_version(renderproto VERSION 0.11.1)
